f627370b03972ead62c41512d8eddeda9ef39197
vigsoft410 <vigsoft410@gmail.com>
2020-02-10 15:08:47 -0300

    Adicionar axios

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import './custom.scss'
 
-
+Vue.use(VueAxios, axios)
 Vue.use(BootstrapVue) 
 Vue.use(IconsPlugin) 
 
